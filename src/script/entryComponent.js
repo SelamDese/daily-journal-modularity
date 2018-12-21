c8a8ec3b8bfe83002abc818a76711f4e58031a4d
selam <selam0315@gmail.com>
2018-12-21 06:51:52 -0600

    html representation of journal entry

--- src/script/entryComponent.js
+++ src/script/entryComponent.js
@@ -1,3 +1,17 @@
 const entryComponent={
-    
+    makeJournalEntryComponent(journalEntry) {
+        //journalEntryStringTemplate=''
+        let journalEntryStringTemplate=
+        `<section>
+            <h3>${journalEntry.concept}</h3>
+            <p>${journalEntry.date}</p>
+            <p>${journalEntry.entry}</p>
+            <p>${journalEntry.mood}</p>
+        </section>`
+        return journalEntryStringTemplate
+    },
+    renderJournalEntries(Entries) {
+        const journalSection = document.querySelector('#journal-section')
+        journalSection.innerHTML += Entries
+    }
 }

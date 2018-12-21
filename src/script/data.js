88f16af38372ec0f66f797c4a91cf4fc1b21d569
selam <selam0315@gmail.com>
2018-12-21 06:49:46 -0600

    fetching entry from api

--- src/script/data.js
+++ src/script/data.js
@@ -1,6 +1,12 @@
-const data = {
-    getJournalEntries () {
-        return fetch("http://localhost:3000/entries")
-            .then(response => response.json())
-    }
+const data={
+    getJournalEntries (){
+        fetch("http://localhost:8088/journalEntries") 
+        .then(entry=>entry.json())  
+        .then(entries => {
+            entries.forEach(entryy=>{
+                let journalEntryy=entryComponent.makeJournalEntryComponent(entryy)
+                entryComponent.renderJournalEntries(journalEntryy)
+                })
+            })
+}
 }
